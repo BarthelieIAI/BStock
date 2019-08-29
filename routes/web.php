d65fd8a191dc1fece0b512d86bd896f4877b7b9c
BarthelieIAI <53558621+BarthelieIAI@users.noreply.github.com>
2019-08-29 09:46:09 +0000

    changement au niveau du fichier web.php
    web.php

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//JE  VOIS AUSSI JONAS
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 /*Route::get('/', function () {
